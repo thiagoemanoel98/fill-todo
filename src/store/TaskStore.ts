e64d28116786b5dafbe4ed3e686b837dfa575877
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-10-22 14:49:03 -0300

    feat: persisting data

--- src/store/TaskStore.ts
+++ src/store/TaskStore.ts
@@ -1,5 +1,7 @@
 import { Task } from '@models';
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type TaskStore = {
   tasks: Task[];
@@ -9,25 +11,31 @@ type TaskStore = {
   removeTask: (task: Task) => void;
 };
 
-export const useTaskStore = create<TaskStore>((set, get) => {
-  return {
-    tasks: [],
-    getTasksNotDone: () => {
-      return get().tasks.filter((item) => item.done === false);
-    },
-    addTask: (task: Task) => {
-      set((state) => ({ tasks: [...state.tasks, task] }));
-    },
-    removeTask: (task: Task) => {
-      set((state) => ({
-        tasks: state.tasks.filter((item) => item.id !== task.id)
-      }));
-    },
-    updateTask: (task: Task) =>
-      set((state) => ({
-        tasks: state.tasks.map((item) =>
-          item.id === task.id ? { ...item, ...task } : item
-        )
-      }))
-  };
-});
+export const useTaskStore = create<TaskStore>()(
+  persist(
+    (set, get) => ({
+      tasks: [],
+      getTasksNotDone: () => {
+        return get().tasks.filter((item) => item.done === false);
+      },
+      addTask: (task: Task) => {
+        set((state) => ({ tasks: [...state.tasks, task] }));
+      },
+      removeTask: (task: Task) => {
+        set((state) => ({
+          tasks: state.tasks.filter((item) => item.id !== task.id)
+        }));
+      },
+      updateTask: (task: Task) =>
+        set((state) => ({
+          tasks: state.tasks.map((item) =>
+            item.id === task.id ? { ...item, ...task } : item
+          )
+        }))
+    }),
+    {
+      name: 'tasks-storage',
+      storage: createJSONStorage(() => AsyncStorage)
+    }
+  )
+);
